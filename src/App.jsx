58e922edfbd0a7d519d744d797c9baf386f81b64
Pop Mihai <105228436+mihai1910@users.noreply.github.com>
2025-10-12 20:18:10 +0300

    Dependencies added project skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import './App.css'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  
+    return(
+    <BrowserRouter>
+   <Nav/>
+   <Header/>
+   <Main/>
+   <Footer/>
+   </BrowserRouter>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './output.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  //npx @tailwindcss/cli -i input.css -o output.css
+  //npx vite
 )
