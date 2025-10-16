2a6afadf3abede2f1dd90534d56e1c7fa80a6ed6
mihai1910 <mihai1910@yahoo.com>
2025-10-16 12:30:03 +0300

    mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Hero from '../components/Hero/Hero'
 import Specials from '../components/Specials/Specials'
 import Reviews from '../components/Reviews/Reviews'
 import About from '../components/About/About'
-import Footer
 
 export default function Home(){
     return(
